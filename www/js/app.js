c30b891a2883a99e9ef35075e139626a61f3cea8
Simon Oulevay <simon.oulevay@gmail.com>
2015-02-27 15:06:34 +0100

    Tabs and routing.

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('citizen-engagement', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,53 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+    // Set up an abstract state for the tabs directive.
+    .state('tab', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'
+    })
+
+    // Set up a state for each tab.
+    .state('tab.newIssue', {
+      url: '/newIssue',
+      views: {
+        'tab-newIssue': {
+          templateUrl: 'templates/newIssue.html'
+        }
+      }
+    })
+
+    .state('tab.issueMap', {
+      url: '/issueMap',
+      views: {
+        'tab-issueMap': {
+          templateUrl: 'templates/issueMap.html'
+        }
+      }
+    })
+
+    .state('tab.issueList', {
+      url: '/issueList',
+      views: {
+        'tab-issueList': {
+          templateUrl: 'templates/issueList.html'
+        }
+      }
+    })
+  ;
+
+  // Define the default state (i.e. the first page displayed when the app opens).
+  $urlRouterProvider.otherwise(function($injector) {
+    $injector.get('$state').go('tab.newIssue');
+  });
+});
